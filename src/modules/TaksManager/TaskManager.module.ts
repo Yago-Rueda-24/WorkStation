ce3fbfe6c1800cf07b0865c57254054aad1b4a97
yago-vasco <yago.rc.04@gmail.com>
2026-03-01 12:36:12 +0100

    Creación del modulo de tareas

--- src/modules/TaksManager/TaskManager.module.ts
+++ src/modules/TaksManager/TaskManager.module.ts
@@ -1,22 +1,28 @@
 
 import { BaseModule } from '../../shared/domain/ports/module.port';
+import { TaskController } from './backend/controllers/task.controller';
+import { TaskService } from './backend/services/task.service';
+import { TaskRepository } from './backend/infraestrucuture/task.repository';
+import { AppDataSource } from '../../core/backend/database';
 
 export class TaskManagerModule extends BaseModule {
     readonly prefix = 'taskmanager';
+    private controller: TaskController;
 
     constructor() {
         super();
+        const repository = new TaskRepository(AppDataSource);
+        const service = new TaskService(repository);
+        this.controller = new TaskController(service);
     }
 
     getHandlers(): Record<string, Function> {
         const handlers: Record<string, Function> = {};
-        /*
-        Object.getOwnPropertyNames(SysInfoController.prototype)
+        Object.getOwnPropertyNames(TaskController.prototype)
             .filter(name => name !== 'constructor')
             .forEach(name => {
                 handlers[name] = (this.controller as any)[name].bind(this.controller);
-            });*/
+            });
         return handlers;
     }
-
 }
